cbea2145e2ea6059df1d632e1e8ef3b01c8663bf
Ivan Yovchev <yovchev23@gmail.com>
2020-04-30 20:59:13 +0200

    Added model inference in example

--- bert_example.py
+++ bert_example.py
@@ -1,20 +1,31 @@
+import torch
+
+from torch.utils.data import DataLoader
+from torch import nn
+
 from transformers import BertModel, BertTokenizer
 from datasets import GCDC_Dataset, collate_pad_fn
-import torch
-from torch.utils.data import DataLoader
+from cnn_model import CNNModel
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
+bert_model = BertModel.from_pretrained('bert-base-uncased')
 
 input_ids = torch.tensor(tokenizer.encode("Hello, Davide is gay.", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
-outputs = model(input_ids)
+outputs = bert_model(input_ids)
 
-data_path = 'data/GCDC/Clinton_train.csv'
-dataset = GCDC_Dataset(data_path, tokenizer)
+data_path = 'data/GCDC/Clinton_test.csv'
+max_len = 300
+
+dataset = GCDC_Dataset(data_path, tokenizer, max_len)
 dataloader = DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=collate_pad_fn)
 
 batch = next(iter(dataloader))
-batch_output = model(batch[0])
+batch_output = bert_model(batch[0])
 
 print(outputs[0].shape)
-print(batch_output[0].shape)
+print(batch_output[0].permute(0,2,1).shape)
+
+n_filters = 128
+classifier = nn.Sequential(nn.Linear(5*n_filters, 1), nn.Sigmoid())
+model = CNNModel(batch_output[0].shape[2], max_len, classifier, n_filters=n_filters)
+print(model(batch_output[0].permute(0,2,1)))
