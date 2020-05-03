1d3e768e20c19e5f5e6b29e39562ac8e2bc91a42
Massimo Spaconi <massimo.spaconi@gmail.com>
2020-05-03 14:47:33 +0200

    training almost working

--- train.py
+++ train.py
@@ -8,20 +8,22 @@ from transformers import BertModel, BertTokenizer
 from datasets import get_dataset, collate_pad_fn
 from cnn_model import CNNModel
 
-def train_model(model, dataloader, loss, optim, device, threshhold = False):
 
+def train_model(model, bert_model, dataloader, loss, optim, device, threshold=False):
     # important for BatchNorm layer
     model.train()
 
     avg_loss = 0
-    for i in range(len(dataloader)):
-        batch = dataloader[i]
-
+    # for i in range(len(dataloader)):
+    #     batch = dataloader[i]
+    for i, (x, labels) in enumerate(dataloader):
         optim.zero_grad()
+        x, labels = x.to(device), labels.to(device)
+        x_ = bert_model(x)
+        out = model(x_[0].permute(0, 2, 1))
 
-        out = model(batch[0].permute(0, 2, 1).to(device))
-
-        grad = loss(out > 0.5 if threshhold else out, batch[1].to(device))
+        # grad = loss(out > 0.5 if threshold else out, labels)
+        grad = loss(out.squeeze(), labels.to(torch.float32))
 
         avg_loss = (avg_loss * i + grad.item()) / (i + 1)
 
@@ -30,8 +32,8 @@ def train_model(model, dataloader, loss, optim, device, threshhold = False):
 
     return avg_loss
 
-def eval_model(model, dataloader, device, threshhold = False):
 
+def eval_model(model, dataloader, device, threshhold=False):
     model.eval()
 
     for i in range(len(dataloader)):
@@ -42,45 +44,44 @@ def eval_model(model, dataloader, device, threshhold = False):
 
     return correct / (len(dataloader) * dataloader.batch_size)
 
-def main(args):
 
+def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     bert_model = BertModel.from_pretrained('bert-base-uncased')
+    bert_model.to(device)
 
     dataset = get_dataset(args.dataset_type, args.train_path, bert_tokenizer, args.max_len)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_pad_fn)
 
     classifier = None
     if args.dataset_type == "gcdc":
-        classifier = nn.Sequential(nn.Linear(5*args.n_filters, 1), nn.Sigmoid())
+        classifier = nn.Sequential(nn.Linear(5 * args.n_filters, 1), nn.Sigmoid())
     # else if
 
-    assert (classifier != None)
+    assert classifier is not None
 
     model = CNNModel(args.embed_size, args.max_len, classifier, device, n_filters=args.n_filters)
 
     loss = None
     if args.dataset_type == "gcdc":
-        loss =  nn.BCELoss()
+        loss = nn.BCELoss()
     # else if
 
     assert (loss != None)
 
     optim = torch.optim.Adam(model.parameters(), args.lr)
 
-    for epoch in args.n_epochs:
-
+    for epoch in range(args.n_epochs):
         print("Epoch: ", epoch)
 
-        avg_loss = train_model(model, dataloader, loss, optim)
+        avg_loss = train_model(model, bert_model, dataloader, loss, optim, device=device)
 
         print("Avg loss: ", avg_loss)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
@@ -94,5 +95,5 @@ if __name__ == "__main__":
     parser.add_argument("--lr", type=float, default=0.005, help="Learning rate")
 
     args = parser.parse_args()
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     main(args)
