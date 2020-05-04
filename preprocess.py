adc152b3f96bca3d0c801b353142cd7449e98ddf
Damyan Kalev <dzkalev@gmail.com>
2020-05-04 22:27:52 +0200

    Splitting hyperpartisan dataset into multiple files

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 import re
+import os
 import pandas as pd
 import argparse
+import json
 import xml.etree.cElementTree as ET
 
 def get_label_data(article_id, label_tree):
@@ -74,54 +76,87 @@ def parse_debate_dataset(filepath):
                                         'Justification', 
                                         'GE', 'LO', 'IS', 'UA', 'UJ', 'Persuasiveness'])
 
+def process_article(elem, train_label_tree, f):
+    article_id = elem.attrib.get('id')
+    title = elem.attrib.get('title')
+    # process article content
+    xml = ET.tostring(elem, encoding='utf-8', method='xml').decode()
+    text = ' '.join(re.findall(r'<p>(.*?)<\/p>', xml))
+    # replace anchor tags and whitespace with single space
+    text = re.sub(r'(<a.*>|</a>|\s{1,})', ' ', text)
+    label, bias, labeled_by = get_label_data(article_id, train_label_tree)
+    return {
+        'article_id': article_id,
+        'label': label,
+        'bias': bias,
+        'labeled_by': labeled_by,
+        'title': title,
+        'text': text,
+    }
+
+def close_hp_file(f):
+    # remove trailing comma and newline
+    f.seek(f.tell()-2, os.SEEK_SET)
+    f.truncate()
+    f.write(']')
+    f.close()
 
 def preprocess_hp_dataset(data_path, labels_path, output_file_path):
     """ Combines data and groundtruth labels xml files in a single tsv file
         Output file format:
             article_id label bias labeled_by title text
     """
-    train_data_tree = ET.iterparse(data_path, events=('start', 'end'))
-    train_label_tree = ET.iterparse(labels_path, events=('start', 'end'))
+    data_tree = ET.iterparse(data_path, events=('start', 'end'))
+    label_tree = ET.iterparse(labels_path, events=('start', 'end'))
 
-    with open(output_file_path, 'w') as f:
-        i = 0
-        root = False
-        for event, elem in train_data_tree:  
-            if event == 'start' and not root:
-                root = elem
-            if event == 'end' and elem.tag == 'article':
-                article_id = elem.attrib.get('id')
-                title = elem.attrib.get('title')
-                # process article content
-                xml = ET.tostring(elem, encoding='utf-8', method='xml').decode()
-                text = ' '.join(re.findall(r'<p>(.*?)<\/p>', xml))
-                # replace anchor tags and whitespace with single space
-                text = re.sub(r'(<a.*>|</a>|\s{1,})', ' ', text)
-                label, bias, labeled_by = get_label_data(article_id, train_label_tree)
-                f.write(f'{article_id}\t{label}\t{bias}\t{labeled_by}\t{title}\t{text}\n')
-                # clear prev elements from memory
-                elem.clear()
-                root.clear()
-                if i != 0 and i % 10000 == 0:
-                    print(f'Articles processed: {i}')
-                i += 1
+    # with open(output_file_path, 'w') as f:
+    max_filesize = args.hp_max_filesize * 1024**2
+    idx_file = 0
+    filepath = f'{output_file_path}-{idx_file}.json'
+    f = open(filepath, 'w')
+    f.write('[')
+    i = 0
+    root = False
+    for event, elem in data_tree:  
+        if event == 'start' and not root:
+            root = elem
+        if event == 'end' and elem.tag == 'article':
+            article = process_article(elem, label_tree, f)
+            json.dump(article, f, indent=2)
+            f.write(',\n')
+            # clear prev elements from memory
+            elem.clear()
+            root.clear()
+            if os.stat(filepath).st_size > max_filesize:
+                close_hp_file(f)
+                idx_file += 1
+                filepath = f'{output_file_path}-{idx_file}.json'
+                f = open(filepath, 'w')
+            if i != 0 and i % 10000 == 0:
+                print(f'Articles processed: {i}')
+            i += 1
+    close_hp_file(f)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess datasets')
     parser.add_argument('--hp_train_data', type=str, help='Hyperpartisan train data', default='data/SemEval/articles-training-20180831.xml')
     parser.add_argument('--hp_train_labels', type=str, help='Hyperpartisan train labels', default='data/SemEval/ground-truth-training-20180831.xml')
-    parser.add_argument('--hp_train_output_file', type=str, help='Hyperpartisan train output file', default='data/SemEval/articles-training.tsv')
+    parser.add_argument('--hp_train_output_file', type=str, help='Hyperpartisan train output file', default='data/SemEval/articles-training')
     parser.add_argument('--hp_valid_data', type=str, help='Hyperpartisan valid data', default='data/SemEval/articles-validation-20180831.xml')
     parser.add_argument('--hp_valid_labels', type=str, help='Hyperpartisan valid labels', default='data/SemEval/ground-truth-validation-20180831.xml')
-    parser.add_argument('--hp_valid_output_file', type=str, help='Hyperpartisan valid output file', default='data/SemEval/articles-valid.tsv')
+    parser.add_argument('--hp_valid_output_file', type=str, help='Hyperpartisan valid output file', default='data/SemEval/articles-valid')
+    parser.add_argument('--hp_max_filesize', type=int, help='Maximum file size in MB for hyperpartisan dataset', default=400)
     parser.add_argument('--debate_datapath', type=str, help='Path to Debate Persuasiveness dataset', default='data/DebatePersuasiveness/DebateArguments.txt')
     parser.add_argument('--debate_output_file', type=str, help='Debate Persuasisveness output file', default='data/DebatePersuasiveness/persuasiveness_dataset.json')
 
     args = parser.parse_args()
 
+    print('Preprocessing hyperpartisan training dataset')
     preprocess_hp_dataset(args.hp_train_data, args.hp_train_labels, args.hp_train_output_file)
+    print('Preprocessing hyperpartisan validation dataset')
     preprocess_hp_dataset(args.hp_valid_data, args.hp_valid_labels, args.hp_valid_output_file)
 
+    print('Preprocessing persuasiveness datasets')
     debates_df = parse_debate_dataset(args.debate_datapath)
     debates_df.to_json(args.debate_output_file, orient='records', indent=2)
