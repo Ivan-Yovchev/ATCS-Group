4abdf9013805063c4239980edbc4f2c6ac9eb0bc
daddabarba <davidebarbieri97@gmail.com>
2020-05-24 19:31:44 +0200

    Reusable output from extractAcc.sh

--- extractAcc.sh
+++ extractAcc.sh
@@ -2,8 +2,4 @@
 
 out=$(cat)
 
-echo 'Per epoch accuracies:'
 echo $out | tr '\r' '\n' | grep -oP 'acc: \K[0-9]*.[0-9]*' | nl
-
-echo 'Final accuracy:'
-echo $out | tr '\r' '\n' | grep -oP 'Final: \K[0-9]*.[0-9]*'
