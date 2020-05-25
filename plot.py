e67b4ab6bb408a5f304d0641ff575c20051fcd73
daddabarba <davidebarbieri97@gmail.com>
2020-05-25 20:32:06 +0200

    Small additions to plot

--- plot.py
+++ plot.py
@@ -7,7 +7,13 @@ data = np.array([float(line.split()[-1]) for line in sys.stdin.readlines()])
 r_avg = np.cumsum(data)/(np.arange(len(data))+1)
 x = list(range(len(data)))
 
-plt.plot(x, data, label = "acc")
-plt.plot(x, r_avg, label = "r_avg")
+plt.plot(x, data, label = "acc", color = "b")
+plt.plot(x, r_avg, label = "r_avg", color = "r", linestyle="dashed")
+
+plt.legend()
+
+plt.title("Validation accuracy over time")
+plt.xlabel("Time-Step")
+plt.ylabel("Running Accuracy")
 
 plt.savefig(sys.argv[1], dpi=900)
